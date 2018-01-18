31fefa92886398d6f31d0bc18f2b4db9f2ef59c5
SheshoVega <hvega.sergio@gmail.com>
2018-01-18 13:08:27 -0600

    agregando project model

---
+++ src/models/project.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const projectSchema = new mongoose.Schema({
+    title:{
+        type: String,
+        required: true
+    },
+    type: {
+        type: String,
+        required: true
+    },
+    tecnologies: {
+        type: [String],
+        required: true
+    },
+    state: {
+        type: String,
+        required: true
+    },
+    product: {
+        type: String,
+        required: true
+    },
+}, { versionKey: false });
+
+const Project = mongoose.model('Project', projectSchema);
+
+function newProject(body) {
+    return new Project({
+        title: body.title,
+        type: body.type,
+        tecnologies: body.tecnologies,
+        state: body.state,
+        product: body.product
+    });
+}
+
+export { Project, newProject };
